bad04ea124d8567a9b14333234b1b717481b34e9
Kevin Jablonka <kevin.jablonka@epfl.ch>
2019-02-12 18:28:32 -0500

    Include stats in setup.py to be able to use bootstrapping

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ description = 'Atomistic Machine-learning Package'
 long_description = open('README').read()
 packages = ['amp', 'amp.descriptor', 'amp.regression', 'amp.model']
 package_dir = {'amp': 'amp', 'descriptor': 'descriptor',
-               'regression': 'regression', 'model': 'model'}
+               'regression': 'regression', 'model': 'model',
+               'stats': 'stats'}
 classifiers = ['Programming Language :: Python',
                'Programming Language :: Python :: 2.6',
                'Programming Language :: Python :: 2.7',
